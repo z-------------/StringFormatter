6f95033dd58f1fc6e415cd5525d4fdd95e52596a
Zack Guard <zacharyguard@gmail.com>
2019-07-18 21:45:17 +0800

    Add more tests

--- src/test/java/UnitTests.java
+++ src/test/java/UnitTests.java
@@ -1,3 +1,5 @@
+import java.util.Map;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -5,6 +7,23 @@ import yy.gourlitburo.stringformatter.StringFormatter;
 
 public class UnitTests {
   private static final StringFormatter formatter = new StringFormatter();
+
+  @Test
+  public void colorize() {
+    final String actual = formatter.colorize("&6Welcome to the server, #{DISPLAYNAME}.");
+    final String expected = "§6Welcome to the server, #{DISPLAYNAME}.";
+    Assert.assertEquals(actual, expected);
+  }
+
+  @Test
+  public void interpolate() {
+    Map<String, String> values = Map.of(
+      "DISPLAYNAME", "lain1998"
+    );
+    final String actual = formatter.interpolate("&6Welcome to the server, #{DISPLAYNAME}.", values);
+    final String expected = "&6Welcome to the server, lain1998.";
+    Assert.assertEquals(actual, expected);
+  } 
 
   @Test
   public void processMarkupNoMix() {
@@ -17,6 +36,16 @@ public class UnitTests {
   public void processMarkupMix() {
     final String actual = formatter.processMarkup("§6**Welcome to the server**, __#{DISPLAYNAME}§4__. I hope you have a §3**great§2** time!");
     final String expected = "§6§lWelcome to the server§r§6, §n#{DISPLAYNAME}§4§r§4. I hope you have a §3§lgreat§2§r§2 time!";
+    Assert.assertEquals(actual, expected);
+  }
+
+  @Test
+  public void format() {
+    Map<String, String> values = Map.of(
+      "FMTNAME", "&llain1998"
+    );
+    final String actual = formatter.format("&6Welcome to the server, #{FMTNAME}&r&6.", values);
+    final String expected = "§6Welcome to the server, §llain1998§r§6.";
     Assert.assertEquals(actual, expected);
   }
 }
